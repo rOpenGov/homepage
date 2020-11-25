b450bd2747a96fee9fab2508636743bf8a0400fd
Pyry Kantanen <pyry.kantanen@utu.fi>
2020-11-25 16:48:36 +0200

    Rebuilding /public/ folder

---
+++ public/scripts/project_tbl.js
@@ -0,0 +1,36 @@
+
+$.ajax({
+    type: "GET",
+    url: "https://api.github.com/orgs/ropengov/repos?per_page=100&sort=updated",
+    dataType: "json",
+    success: function(result) {
+    var whitelist = ['homepage','geofi','hetu','eurostat','rqog','fmi2','pxweb','openthl','sweidnumber','regions','sotkanet','vipunen','digitransit','europarl']
+        for( i in result ) {
+
+        // homepage if exists
+            if (result[i].homepage == null){
+               homepage = ''
+               homepage_icon = ''
+            } else
+            {
+                homepage = result[i].homepage
+                homepage_icon = '<i class="fa fa-home" aria-hidden="true"></i>'
+            }
+
+     if (whitelist.includes(result[i].name)) {            
+            $("#repo_tbl").append(
+            "<tr> \
+            <td><a href='https://github.com/ropengov/" + result[i].name + "'>" + result[i].name + "</a></td> \
+            <td><i>"  + result[i].description +  "<i></td> \
+            <td><a href = '"  + homepage +  "'>" + homepage_icon + "</a></td> \
+            <td>" + result[i].forks + "</td> \
+            <td>" + result[i].open_issues + "</td> \
+            <td>" + result[i].stargazers_count + "</td> \
+            <td>" + result[i].updated_at + "</td> \
+            </tr>");
+            console.log("i: " + i);
+        }
+        console.log(result);
+        }
+    }
+  });
