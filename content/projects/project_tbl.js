5f84ae0cfaf3b5eba00b6b3beca3f3c5b36ed527
Markus Kainu <markus.kainu@kapsi.fi>
2021-05-04 11:28:45 +0300

    Helsinki added to list of pkgs

--- content/projects/project_tbl.js
+++ content/projects/project_tbl.js
@@ -4,16 +4,16 @@ $.ajax({
     url: "https://api.github.com/orgs/ropengov/repos?per_page=100&sort=updated",
     dataType: "json",
     success: function(result) {
-	var whitelist = ['homepage','geofi','hetu','eurostat','rqog','fmi2','pxweb','openthl','sweidnumber','regions','sotkanet','vipunen','digitransit','europarl', 'iotables', 'psData', 'pollstR', 'rwfs', 'hansard', 'europarl', 'RPublica', 'dkstat', 'usbroadband', 'mpg', 'federalregister', 'ogdindiar']
+	var whitelist = ['homepage','geofi','hetu','eurostat','rqog','fmi2','pxweb','openthl','sweidnumber','regions','sotkanet','vipunen','digitransit','europarl', 'iotables', 'psData', 'pollstR', 'rwfs', 'hansard', 'europarl', 'RPublica', 'dkstat', 'usbroadband', 'mpg', 'federalregister', 'ogdindiar','helsinki']
 
 	// edu,
 	// eurostat_geodata
-	
+
 	// In devel:
 	// helsinki, sorvi, election
-	
+
         // Remove: fmi, edudata, rustfare
-	
+
         for( i in result ) {
 
         // homepage if exists
@@ -26,7 +26,7 @@ $.ajax({
                 homepage_icon = '<i class="fa fa-home" aria-hidden="true"></i>'
             }
 
-     if (whitelist.includes(result[i].name)) {            
+     if (whitelist.includes(result[i].name)) {
             $("#repo_tbl").append(
             "<tr> \
             <td><a href='https://github.com/ropengov/" + result[i].name + "'>" + result[i].name + "</a></td> \
