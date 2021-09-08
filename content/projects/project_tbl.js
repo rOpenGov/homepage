1e8375357fc61ff2c1794b12f5ca154cc4d5a6f2
Pyry Kantanen <pyry.kantanen@gmail.com>
2021-09-08 17:05:56 +0300

    Add giscoR, retroharmonize to projects list

--- content/projects/project_tbl.js
+++ content/projects/project_tbl.js
@@ -4,13 +4,13 @@ $.ajax({
     url: "https://api.github.com/orgs/ropengov/repos?per_page=100&sort=updated",
     dataType: "json",
     success: function(result) {
-	var whitelist = ['homepage','geofi','hetu','eurostat','rqog','fmi2','pxweb','openthl','sweidnumber','regions','sotkanet','vipunen','digitransit','europarl', 'iotables', 'psData', 'pollstR', 'rwfs', 'hansard', 'europarl', 'RPublica', 'dkstat', 'usbroadband', 'mpg', 'federalregister', 'ogdindiar','helsinki']
+	var whitelist = ['homepage','geofi','hetu','eurostat','rqog','fmi2','pxweb','openthl','sweidnumber','regions','sotkanet','vipunen','digitransit','europarl', 'iotables', 'psData', 'pollstR', 'rwfs', 'hansard', 'europarl', 'RPublica', 'dkstat', 'usbroadband', 'mpg', 'federalregister', 'ogdindiar','helsinki', 'giscoR', 'retroharmonize']
 
 	// edu,
 	// eurostat_geodata
 
 	// In devel:
-	// helsinki, sorvi, election
+	// sorvi, election
 
         // Remove: fmi, edudata, rustfare
 
