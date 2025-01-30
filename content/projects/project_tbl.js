7f58149dcd3016282dea58a899747de5cb5a8724
Diego H. <diego.hernangomezherrero@gmail.com>
2025-01-30 13:39:43 +0100

    Remove archived packages
    No longer maintained / not working packages (API broken/not available)

--- content/projects/project_tbl.js
+++ content/projects/project_tbl.js
@@ -4,7 +4,7 @@ $.ajax({
     url: "https://api.github.com/orgs/ropengov/repos?per_page=100&sort=updated",
     dataType: "json",
     success: function(result) {
-	var whitelist = ['homepage','geofi','hetu','eurostat','rqog','fmi2','pxweb','openthl','sweidnumbr','regions','sotkanet','vipunen','digitransit','europarl', 'iotables', 'psData', 'pollstR', 'rwfs', 'hansard', 'europarl', 'RPublica', 'dkstat', 'usbroadband', 'mpg', 'federalregister', 'ogdindiar','helsinki', 'giscoR', 'retroharmonize', 'usdoj', 'oldbailey', 'kelaopendata', 'r311', 'sorvi', 'geodk', 'finna']
+	var whitelist = ['homepage','geofi','hetu','eurostat','rqog','fmi2','pxweb','openthl','sweidnumbr','regions','sotkanet','vipunen','digitransit','europarl', 'iotables', 'psData', 'rwfs', 'hansard', 'europarl', 'RPublica', 'dkstat', 'mpg', 'federalregister', 'ogdindiar','helsinki', 'giscoR', 'retroharmonize', 'usdoj', 'oldbailey', 'kelaopendata', 'r311', 'sorvi', 'geodk', 'finna']
 
 	// edu,
 	// eurostat_geodata
@@ -13,6 +13,7 @@ $.ajax({
 	// election
 
         // Remove: fmi, edudata, rustfare
+	// Archived (removed): pollstR, usbroadband
 
         for( i in result ) {
 
